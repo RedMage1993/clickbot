3a70418a98369bf98f1d327a40dfc43758317039
Fritz Ammon <redmage1993@gmail.com>
2019-03-04 22:47:33 -0800

    attempting to get the clicks working

--- clickbot/main.cpp
+++ clickbot/main.cpp
@@ -46,29 +46,34 @@ struct MouseClick {
 vector<MouseClick> recorded;
 DWORD lastMouseClickTime = 0;
 
+void inputFromMouseClick(const MouseClick& mouseClick, INPUT& input);
+void sendInputFromMouseClicks();
+
 int main()
 {
-	char enter;
+	MSG msg;
 
 	srand(static_cast<unsigned int> (time(reinterpret_cast<time_t*> (NULL))));
 
 	cout << "Clickbot by Fritz Ammon.\n\n";
 	cout << std::boolalpha;
 
-	cout << "  F2 to start recording clicks, F4 to playback, F6 to interrupt playback\n";
-	cout << "  Press Enter to shut down\n";
+	cout << "  F2 to start recording clicks, F4 to playback\n";
 
 	hRecordProc = CreateThread(0, 0, recordProc, 0, 0, 0);
 	hPlaybackProc = CreateThread(0, 0, playbackProc, 0, 0, 0);
 
-	cin >> enter;
-	cout << endl;
+	installHook();
 
-	cin.clear();
-	cin.ignore(200, '\n');
+	while (GetMessage(&msg, 0, 0, 0) != 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 
-	closeThread(hRecordProc);
-	closeThread(hPlaybackProc);
+	//uninstallHook();
+	//closeThread(hRecordProc);
+	//closeThread(hPlaybackProc);
 
 	return 0;
 }
@@ -93,19 +98,27 @@ LRESULT CALLBACK LowLevelMouseProc(int nCode, WPARAM wParam, LPARAM lParam)
 		}
 
 		lastMouseClickTime = msllHookStruct->time;
+
+		recorded.push_back(mouseClick);
+
+		cout << "\nRecorded " << ((mouseClick.type == WM_LBUTTONDOWN) ? "left " : "right ") << "click (" << mouseClick.pt.x << ", " << mouseClick.pt.y << ") at " << lastMouseClickTime;
 	}
 
-	return CallNextHookEx(hLLMouseHook, nCode, wParam, lParam);
+	return CallNextHookEx(0, nCode, wParam, lParam);
+}
+
+void clearRecordedMouseClicks()
+{
+	recorded.clear();
+	lastMouseClickTime = 0;
+	cout << "\nCleared recorded mouse clicks";
 }
 
 void installHook()
 {
 	if (hLLMouseHook != 0) { return; }
 
-	recorded.clear();
-	lastMouseClickTime = 0;
-
-	hLLMouseHook = SetWindowsHookEx(WH_MOUSE_LL, LowLevelMouseProc, 0, 0);
+	hLLMouseHook = SetWindowsHookEx(WH_MOUSE_LL, LowLevelMouseProc, GetModuleHandle(0), 0);
 }
 
 void uninstallHook()
@@ -165,19 +178,28 @@ DWORD WINAPI recordProc(LPVOID lpParameter)
 
 		switch (state) {
 		case idling:
-			installHook();
+			/*installHook();*/
+			clearRecordedMouseClicks();
 			state = recording;
+			cout << "\nRecording...";
 			break;
 		case recording:
-			uninstallHook();
+			/*uninstallHook();*/
 			state = idling;
+			cout << "\nIdling...";
 			break;
 		case playing:
 			closeThread(hPlaybackCoreProc);
-			installHook();
+			clearRecordedMouseClicks();
+			/*installHook();*/
 			state = recording;
+			cout << "\nRecording...";
 			break;
 		}
+
+		improveSleepAcc(true);
+		Sleep(1000);
+		improveSleepAcc(false);
 	}
 
 	return 0;
@@ -203,33 +225,89 @@ DWORD WINAPI playbackProc(LPVOID lpParameter)
 			if (recorded.size() == 0) { break; }
 			hPlaybackCoreProc = CreateThread(0, 0, playbackCoreProc, 0, 0, 0);
 			state = playing;
+			cout << "\nPlaying...";
 			break;
 		case recording:
-			uninstallHook();
+			/*uninstallHook();*/
 			hPlaybackCoreProc = CreateThread(0, 0, playbackCoreProc, 0, 0, 0);
 			state = playing;
+			cout << "\nPlaying...";
 			break;
 		case playing:
 			closeThread(hPlaybackCoreProc);
+			state = idling;
+			cout << "\nIdling...";
 			break;
 		}
+
+		improveSleepAcc(true);
+		Sleep(1000);
+		improveSleepAcc(false);
 	}
 
 	return 0;
 }
 
+void inputFromMouseClick(const MouseClick& mouseClick, INPUT& input)
+{
+	DWORD extraInfo = GetMessageExtraInfo();
+	LONG screenWidth = GetSystemMetrics(0);
+	LONG screenHeight = GetSystemMetrics(1);
+	const LONG ABSOLUTE_MAX_COORDINATE = 65535;
+
+	SecureZeroMemory(&input, sizeof(INPUT));
+
+	input.type = INPUT_MOUSE;
+
+	input.mi.dwExtraInfo = extraInfo;
+
+	// Normalization
+	input.mi.dx = static_cast<double> (mouseClick.pt.x) / screenWidth * ABSOLUTE_MAX_COORDINATE;
+	input.mi.dy = static_cast<double> (mouseClick.pt.y) / screenHeight * ABSOLUTE_MAX_COORDINATE;
+
+	input.mi.dwFlags = MOUSEEVENTF_ABSOLUTE;
+}
+
+void sendInputFromMouseClicks()
+{
+	INPUT inputs[3];
+
+	for (int i = 0; i < recorded.size(); i++) {
+		improveSleepAcc(true);
+		Sleep(recorded[i].wait);
+		improveSleepAcc(false);
+
+		inputFromMouseClick(recorded[i], inputs[0]);
+		inputs[0].mi.dwFlags |= MOUSEEVENTF_MOVE;
+
+		inputFromMouseClick(recorded[i], inputs[1]);
+		inputs[1].mi.dwFlags |= (recorded[i].type == WM_LBUTTONDOWN) ? MOUSEEVENTF_LEFTDOWN : MOUSEEVENTF_RIGHTDOWN;
+
+		inputFromMouseClick(recorded[i], inputs[2]);
+		inputs[2].mi.dwFlags |= (recorded[i].type == WM_LBUTTONDOWN) ? MOUSEEVENTF_LEFTUP : MOUSEEVENTF_RIGHTUP;
+
+		SendInput(3, inputs, sizeof(INPUT));
+
+		cout << "\nClicking at (" << recorded[i].pt.x << ", " << recorded[i].pt.y << ")";
+	}
+}
+
 DWORD WINAPI playbackCoreProc(LPVOID lpParameter)
 {
+	LPINPUT inputs = 0;
 	UNREFERENCED_PARAMETER(lpParameter);
 
 	if (recorded.size() == 0) {
 		state = idling;
-		return;
+		cout << "\nIdling...";
+		return 0;
 	}
 
-	// playback recorded clicks
+	cout << "\nPlaying back " << recorded.size() << " mouse events";
+
+	sendInputFromMouseClicks();
 
 	state = idling;
-
+	cout << "\nIdling...";
 	return 0;
 }
